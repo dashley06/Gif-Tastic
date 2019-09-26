b168c57041600d19dd8e9cf5f8a4863a0eed6dbd
Ashley Clark <debashley06@gmail.com>
2019-09-26 17:53:25 -0400

    start user controls, and animate and still

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,24 @@
+$( document ).ready(function() {
+
 //arrary of string for the variable topics
 var topics = ["Hawaii", "Disney World", "Paris", "London", "San Diego", "Jamaica", "Thailand", "China", "Texas", "New York", "Bahamas", "Brazil", "Carnival Cruise"];
 
 //function to create buttons for each of the topic strings of the array
 function travelButtonsCreate (){
     for (var t = 0; t < topics.length; t++){
+        //create new button element in HTML for each string
          var a = $("<button>");
-         a.addClass("city");
+         a.addClass("cities");
          a.attr("data-city", topics[t]);
          a.text(topics[t]);
-         $("#travelButtons").prepend(a);
+         $("#travelButtons").append(a);
     }
 }
 travelButtonsCreate();
 
+function findTheGifs(){
 //assigning variable for each on-click events for buttons with the attribute of data-city
-$("button").on("click", function(){
+    $("button").on("click", function(){
     var city = $(this).attr("data-city");
     
 //assigment of variable for query URL
@@ -40,10 +44,60 @@ $("button").on("click", function(){
         var imageDisplay = $("<img>");
 
         imageDisplay.attr("src", results[g].images.fixed_height_still.url);
-        gifDisplay.prepend(imageDisplay);
+
         gifDisplay.prepend(ratingDisplay);
+        gifDisplay.prepend(imageDisplay);
         $("#displayGifs").prepend(gifDisplay);
     }
 });
 });
+}
+findTheGifs();
 
+//function to allow for clicking on images that will toggle from animated to still
+function animateGif(){
+ 
+    //how to assign "this" to each gif
+    var state = $(this).attr("data-state");
+        //!!!!!!!!!!!!!!! console.log(this);
+
+//switch animate to stilll and still to animate
+    if (state==="still"){
+        $(this).attr("data-still", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));  
+    } else {
+        $(this).attr("data-animate", "still");
+        $(this).attr("src", $(this).attr("data-still"));  
+    }
+}
+animateGif();
+
+//--------------------------------------------------------------------------------------------------------------
+//USER INPUT
+//user can input a city to add to the array
+        $("#userButton").on("click", function(event){
+            event.preventDefault();
+            userControls();
+
+function userControls(){
+    
+//variable to hold value of the user input box, trim away white space
+        var userChoice = $("#userInput").val().trim();
+
+//add value of user input box to the end of the topics array
+        topics.push(userChoice);
+        //console.log(topics);
+        $("#travelButtons").empty();
+
+
+//assign the same attribute to the user's guess from the input box
+        userChoice = $(this).attr("data-city");   
+}
+
+travelButtonsCreate(); 
+findTheGifs();
+   
+})
+
+
+});
